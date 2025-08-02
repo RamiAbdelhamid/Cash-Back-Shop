6d04a47af7ddac96ec01b81ba42756389fc1324a
Rami Abdelhamid <ramighassan10@gmail.com>
2025-08-02 19:09:54 +0300

    edit vite.config

--- Shop/vite.config.js
+++ Shop/vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// ✅ هذا التعديل المهم
 export default defineConfig({
-  plugins: [react() , tailwindcss()], 
+  base: './',
+  plugins: [react(), tailwindcss()],
 })
